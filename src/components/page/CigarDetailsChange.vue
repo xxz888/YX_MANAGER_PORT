68dbd19e4096b322ed3698a49a232f5e41e5e0e7
xxz888 <6952731@qq.com>
2019-03-04 00:31:37 +0800

    no message

--- src/components/page/CigarDetailsChange.vue
+++ src/components/page/CigarDetailsChange.vue
@@ -61,6 +61,17 @@
 
 
     </el-form>
+
+        <el-upload
+                action="https://jsonplaceholder.typicode.com/posts/"
+                list-type="picture-card"
+                :on-preview="handlePictureCardPreview"
+                :on-remove="handleRemove">
+            <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
     </div>
 </template>
 
@@ -88,7 +99,9 @@
                   price_box_overswas: '',
                   box_size: '',
                   collect_number: '',
-              }
+              },
+              dialogImageUrl: '',
+              dialogVisible: false
           }
         },
         created(){
@@ -98,6 +111,13 @@
             getParams(){
                 this.form = this.$route.query;
             },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePictureCardPreview(file) {
+                this.dialogImageUrl = file.url;
+                this.dialogVisible = true;
+            }
         }
     }
 </script>
